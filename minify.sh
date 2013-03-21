e14dd57a82f73af7a1bd435b82a42363ef453b8b
Felipe Riveroll Aguirre <friveroll@gmail.com>
2013-03-21 15:41:19 -0600

    added code for acerca page

--- minify.sh
+++ minify.sh
@@ -51,3 +51,14 @@ java -jar yuicompressor-2.4.7.jar -o js/sensores.min.js js/sensores.min.js
 
 #https://npmjs.org/package/json-minify
 json-minify js/sensores.json > js/sensores.min.json
+
+cat \
+	vendor/js/jquery-1.9.1.js \
+	vendor/js/bootstrap.js \
+	js/page.js \
+	vendor/js/swfobject.js
+	vendor/js/popcorn-complete.min.js \
+	vendor/js/popcorn.slideshare.js \
+	js/acerca.js \
+	> js/acerca.min.js
+java -jar yuicompressor-2.4.7.jar -o js/acerca.min.js js/acerca.min.js
