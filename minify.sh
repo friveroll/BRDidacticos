6b8f4b8a0c68e2150c0c0751e5fa44c2a2a61198
Felipe Riveroll Aguirre <friveroll@gmail.com>
2013-03-21 13:51:36 -0600

    added sensores.min.json

--- minify.sh
+++ minify.sh
@@ -11,7 +11,7 @@ cat \
 	vendor/css/bootstrap.css \
 	vendor/css/bootstrap-responsive.css \
 	vendor/css/font-awesome.css \
-	vendor/css/custom.css \
+	css/custom.css \
 	css/producto.css \
 	> css/producto.min.css
 java -jar yuicompressor-2.4.7.jar -o css/producto.min.css css/producto.min.css
@@ -28,7 +28,7 @@ java -jar yuicompressor-2.4.7.jar -o css/sensores.min.css css/sensores.min.css
 cat \
 	vendor/js/jquery-1.9.1.js \
 	vendor/js/bootstrap.js \
-	vendor/js/page.js \
+	js/page.js \
 	> js/master.min.js
 java -jar yuicompressor-2.4.7.jar -o js/master.min.js js/master.min.js
 
@@ -44,9 +44,10 @@ cat \
 	vendor/js/bootstrap.js \
 	vendor/js/underscore.js \
 	vendor/js/backbone.js \
-	vendor/js/bbGrid/bbGrid.js \
+	vendor/js/bbGrid.js \
 	js/sensores.js \
 	> js/sensores.min.js
 java -jar yuicompressor-2.4.7.jar -o js/sensores.min.js js/sensores.min.js
 
-java -jar yuicompressor-2.4.7.jar -o vendor/json/sensores.json js/sensores.min.json
+#https://npmjs.org/package/json-minify
+json-minify js/sensores.json > js/sensores.min.json
