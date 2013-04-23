0fa1361a13a560610689f27ea57d162b98426d5c
Felipe Riveroll Aguirre <friveroll@gmail.com>
2013-04-23 01:44:22 -0500

    Corrected bug to rename

--- rename.sh
+++ rename.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Renaming files..."
-mv ./larasys/application/config/auth.default.php /larasys/application/config/auth.php
-mv ./larasys/application/config/database.default.php /larasys/application/config/database.php
-mv ./larasys/application/config/session.default.php /larasys/application/config/session.php
-mv ./larasys/bundles/messages/config/config.default.php /larasys/bundles/messages/config/config.php
+mv ./larasys/application/config/auth.default.php ./larasys/application/config/auth.php
+mv ./larasys/application/config/database.default.php ./larasys/application/config/database.php
+mv ./larasys/application/config/session.default.php ./larasys/application/config/session.php
+mv ./larasys/bundles/messages/config/config.default.php ./larasys/bundles/messages/config/config.php
 
 ## Checkout the changes to git
 git checkout ./larasys/application/config/auth.default.php
